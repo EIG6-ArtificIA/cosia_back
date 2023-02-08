e4e42366d3ed32341e20a31122c72610e93c5ff7
maxi_malain <maxime.allain.8@gmail.com>
2023-02-08 17:42:04 +0100

    services: create intersections point method

--- predictions_map/services.py
+++ predictions_map/services.py
@@ -1,6 +1,9 @@
 from predictions_map.models import RASTER_VALUE_TO_LABEL, PredictedArea
 
 
-def getAllObjectsFromType(type):
+def getAllPredictedAreasFromType(type):
     raster_val = list(RASTER_VALUE_TO_LABEL.keys())[list(RASTER_VALUE_TO_LABEL.values()).index(type)]
     return PredictedArea.objects.filter(raster_val=raster_val)
+
+def getAllPredictedAreasIntersectingPoint(point):
+    return PredictedArea.objects.filter(geom__intersects=point)
