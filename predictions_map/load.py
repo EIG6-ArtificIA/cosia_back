23b2d4fe8c0fe0139f5ba0a786e8bb9d7d3c4b3d
maxi_malain <maxime.allain.8@gmail.com>
2023-08-28 17:21:02 +0200

    feat: update PredictedArea model + load

--- predictions_map/load.py
+++ predictions_map/load.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 from django.contrib.gis.utils import LayerMapping
-from .models import  PredictionsMap
+from .models import PredictedArea
 
 predictionsmap_mapping = {
-    'raster_val': 'raster_val',
-    'geom': 'MULTIPOLYGON',
+    'name': 'nom',
+    'geom': 'POLYGON',
 }
 
-predictionsmap_shp = Path(__file__).resolve().parent / 'data' / 'data_Aubiet_25_05.shp'
+predictionsmap_shp = Path(__file__).resolve().parent / 'data' / 'Saint-Nazaire' / 'emprise_SaintNazaire_agglo.shp'
 
 def run(verbose=True):
-    lm = LayerMapping(PredictionsMap, predictionsmap_shp, predictionsmap_mapping, transform=False)
+    lm = LayerMapping(PredictedArea, predictionsmap_shp, predictionsmap_mapping, transform=False)
     lm.save(strict=True, verbose=verbose)
--- predictions_map/models.py
+++ predictions_map/models.py
@@ -1,30 +1,7 @@
 from django.contrib.gis.db import models
 
-RASTER_VALUE_TO_LABEL = {
-    0:"Batiment",
-    1:"Zone permeable",
-    2:"Zone impermeable",
-    3:"Piscine",
-    4:"Sol nu",
-    5:"Surface Eau",
-    6:"Neige",
-    7:"Conifere",
-    8:"Coupe",
-    9:"Feuillu",
-    10:"Broussaille",
-    11:"Vigne",
-    12:"Culture",
-    13:"Terre labouree",
-    15:"Autre",
-    17:"Pelouse",
-    18:"Serre",
-    19:"Autre",
-}
 
 class PredictedArea(models.Model):
-    raster_val = models.BigIntegerField()
+    name = models.CharField(max_length=200, null=False, blank=False)
     geom = models.MultiPolygonField(srid=2154)
-
-    @property
-    def type(self):
-        return RASTER_VALUE_TO_LABEL[self.raster_val]
+    status = models.CharField(max_length=100, null=False, blank=False)
