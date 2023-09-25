f6ad6b9558384143e321f17bb67aade510886804
maxi_malain <maxime.allain.8@gmail.com>
2023-09-25 16:22:19 +0200

    devops: update docker configuration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,14 +4,18 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
+echo 'Running migrations...'
 # python3 manage.py flush --no-input
 python3 manage.py migrate
+
+echo 'Collecting static files...'
+python3 manage.py collectstatic --no-input
 
 exec "$@"
