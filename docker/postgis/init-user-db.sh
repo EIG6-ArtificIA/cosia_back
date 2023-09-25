9c2694873b49ecec0c9e6315ea21561543cc3516
maxi_malain <maxime.allain.8@gmail.com>
2023-09-25 16:43:43 +0200

    devops: enhancement

--- docker/postgis/init-user-db.sh
+++ docker/postgis/init-user-db.sh
@@ -3,7 +3,4 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE cosia_db;
-	CREATE USER docker;
-	CREATE DATABASE docker;
-	GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
 EOSQL
