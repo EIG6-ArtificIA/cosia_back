fc058c763fdb7c40ea5430ebdc7cc1becef36591
Maxime Allain <maxime.allain.8@gmail.com>
2023-10-17 18:34:50 +0200

    docker: new image + allowed hosts

--- cosia_back/settings.py
+++ cosia_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_@zjr+@)kwv@(b^i0)8c^3x(487fcuh#28q^48xxx!##gq#ia$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.2.0.67"]
 
 # Application definition
 
