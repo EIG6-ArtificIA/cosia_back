6001375de883dbbedef46c8de40d44a56284f51d
Maxime Allain <maxime.allain.8@gmail.com>
2023-10-25 13:23:18 +0200

    enhance tests

--- predictions_map/tests/test_api.py
+++ predictions_map/tests/test_api.py
@@ -24,16 +24,8 @@ DEPARTMENT_DATA_SERIALIZER_SCHEMA = {
 
 class DepartmentApiTestCase(APITestCase):
     def setUp(self):
-        code_dor = DepartmentFactory(name="Côte d'Or", number="21")
-        finistere = DepartmentFactory(name="Finistère", number="29")
-        manche = DepartmentFactory(name="Manche", number="50")
-        DepartmentFactory()
-
-        DepartmentDataFactory(
-            department=code_dor, year=2008, download_link="http://rigo.lo"
-        )
-        DepartmentDataFactory(department=finistere)
-        DepartmentDataFactory(department=manche)
+        self.code_dor = DepartmentFactory(name="Ain", number="01")
+        DepartmentFactory.create_batch(3)
 
     def test_get_departments(self):
         response = self.client.get("/api/departments/", format="json")
@@ -43,8 +35,32 @@ class DepartmentApiTestCase(APITestCase):
         data = json.loads(response.content)
         self.assertEqual(len(data), 4)
 
-        first_element = data[0]
-        self.assertTrue(check_structure(first_element, DEPARTMENT_SERIALIZER_SCHEMA))
+        ain = list(filter(lambda dep: dep["name"] == "Ain", data))[0]
+        self.assertTrue(check_structure(ain, DEPARTMENT_SERIALIZER_SCHEMA))
+
+        self.assertEquals(
+            ain,
+            {
+                "id": self.code_dor.id,
+                "name": "Ain",
+                "number": "01",
+                "status": "not_available",
+                "geom": str(self.code_dor.geom),
+            },
+        )
+
+
+class DepartmentDownloadApiTestCase(APITestCase):
+    def setUp(self):
+        code_dor = DepartmentFactory(name="Côte d'Or", number="21")
+        finistere = DepartmentFactory(name="Finistère", number="29")
+        manche = DepartmentFactory(name="Manche", number="50")
+
+        DepartmentDataFactory(
+            department=code_dor, year=2008, download_link="http://rigo.lo"
+        )
+        DepartmentDataFactory(department=finistere)
+        DepartmentDataFactory(department=manche)
 
     def test_get_data_departments(self):
         response = self.client.get("/api/department_data/", format="json")
